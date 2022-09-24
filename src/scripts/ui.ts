852cc148c76835d66ca157e3033ad34d33ca741e
Sebastian Macke <sebastian@macke.de>
2022-09-24 16:53:46 +0200

    Improved error handling

--- src/scripts/ui.ts
+++ src/scripts/ui.ts
@@ -40,10 +40,10 @@ function ShowError(message: string, e: Error) {
 
     errorObject.style.color = "#dc3545"
 
-    errorObject.innerHTML = message
-    errorObject.innerHTML += "\n"
+    errorObject.innerHTML = "\n" + message
+    errorObject.innerHTML += "\n\n"
     errorObject.innerHTML += e.message
-    errorObject.innerHTML += "\n"
+    errorObject.innerHTML += "\n\n"
     errorObject.innerHTML += e.stack
     errorObject.innerHTML += "<br><strong>As of September 2022, only the <a href=\"https://www.google.com/intl/en/chrome/dev/\">Chrome unstable browser </a>with enabled WebGPU is supported.</strong>"
 
