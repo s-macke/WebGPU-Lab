25badafda1735f587872f6b3df7a4b4d40bbf174
Sebastian Macke <sebastian@macke.de>
2022-09-24 16:19:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 set -e
+npm install
 npm run prestart:copysrc
 npm run prestart:copyassets
 npx tsc -noEmit
 npx esbuild src/scripts/ui.ts --bundle --outfile=build/ui.js
-#cp node_modules/@webgpu/glslang/dist/web-devel/glslang.wasm build/src/
 
 #on first start run "npm install"
 #npm outdated
