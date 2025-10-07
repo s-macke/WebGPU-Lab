eaf45b5b377ed8b72eda8beb9c8a2b9282de69d1
Sebastian Macke <sebastian@macke.de>
2025-10-07 21:39:11 +0200

    Use relative base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
     root: 'src',
+    base: '', // use relative references
     publicDir: '../assets',
     plugins: [
         glsl({
